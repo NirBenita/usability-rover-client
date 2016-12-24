4e59485083c453ccc1f9af22e0722564f4fb1f1b
NirBenita <benitanir@gmail.com>
2016-12-24 16:31:18 +0200

    styled session page some more

--- src/app.js
+++ src/app.js
@@ -5,9 +5,25 @@ const defaultTheme = {
   systemFonts: '"-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;'
 };
 
+const FlexContainer = styled.div`
+  display:flex;
+
+  > * {
+    flex:1;
+  }
+`
+
+const SessionInfo = styled.section`
+  max-width:276px;
+  height:100%;
+  padding:12px;
+`
+
 const Message = styled.li`
   font-size: 1rem;
-  background-color: Cornsilk;
+  background-color: white;
+  border-radius:6px;
+  margin-bottom:6px;
   color: SlateGrey;
   font-family: ${props => props.theme.systemFonts};
   padding:12px;
@@ -15,7 +31,7 @@ const Message = styled.li`
 
 const MessageStack = styled.ul`
   list-style: none;
-  padding:0;
+  padding:12px;
 `;
 
 export default class App extends React.Component {
@@ -23,15 +39,17 @@ export default class App extends React.Component {
     const session = this.props.session
     return (
       <ThemeProvider theme={defaultTheme}>
-        <div>
-          <h1>{session.name}</h1>
-          <p>Started at {new Date(session.startTime).toString}, lasted {(Math.floor((session.startTime - session.endTime)/1000))} seconds</p>
+        <FlexContainer>
+          <SessionInfo>
+            <h1>{session.name}</h1>
+            <p>Started at {new Date(parseInt(session.startTime)).toString()}</p>
+            <p>Lasted {(Math.floor((session.startTime - session.endTime) / 1000))} seconds</p>
+          </SessionInfo>
           <MessageStack>
             {session.messages.map(msg => <Message>{msg.text}</Message>)}
           </MessageStack>
-        </div>
+        </FlexContainer>
       </ThemeProvider>
-      );
+    );
   }
 }
-
